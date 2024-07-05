5da0208f98c39f8f05d40f4a1cf46955a0e83175
Zahid <zahid.hzade001@gmail.com>
2024-07-05 19:26:53 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+let DATA = null
+
+const ajax = new XMLHttpRequest()
+const main = document.querySelector("main")
+
+loadPage(location.hash.slice(1) || 'main')
+
+function loadPage(page) {
+    ajax.open('GET', `content/${page}.html`)
+    ajax.send()
+    ajax.onload = () => main.innerHTML = ajax.responseText
+}
+
+const sidebar = document.getElementById("sidebar")
+
+function getData() {
+    axios.get('https://api.mirafgan.me/neptun/catalog')
+        .then(res => {
+            DATA = res.data
+            console.log(DATA)
+            showCategory(DATA)
+        })
+}
+getData()
+
+function sideOpen(){
+    sidebar.classList.toggle('sidebar-active')
+}
+
+function showCategory(data) {
+    const leftmenulists = document.getElementById("left-menu-lists")
+    data.forEach(item => {
+        let submenuitems = item.submenu.map(element => `<li><a href="#" class="menu-links">${element.name}</a></li>` ).join('')
+
+        leftmenulists.innerHTML += `<li>
+                                        <div class="cat-icon">
+                                            <img src="${item.icon}" />
+                                        </div>
+                                        <a class="menu-links" href="#">${item.name}</a>
+                                        <div class="menu-sub">
+                                            <ul class="${item.submenu.length ? '' : 'none'}">
+                                                ${submenuitems}
+                                            </ul>
+                                        </div>
+                                    </li>` 
+    });
+}
+
---
+++ js/slider.js
@@ -0,0 +1,48 @@
+$(() => {
+    const image = ['slider-image1.webp', 'slider-image2.webp', 'slider-image3.webp']
+    const path = '../img/'
+    let x = 0
+    let timer = null
+    const slide = $('.slider-item')
+
+    timer = setInterval(change, 0, 0)
+
+    $('.slider-pagination-item').eq(0).addClass('pag-active')
+
+    function change(dir) {
+        clearInterval(timer)
+        x += dir
+        if (x > image.length - 1) x = 0
+        if (x < 0) x = image.length - 1
+        slide.fadeOut(function () {
+            slide.css({ background: `url('${path + image[x]}')center/cover` })
+            slide.fadeIn()
+        })
+        $('.slider-pagination-item').removeClass('pag-active')
+        $('.slider-pagination-item').eq(x).addClass('pag-active')
+        timer = setInterval(change, 5000, 1)
+    }
+
+    $('.slider-pagination-item').click(function () {
+        clearInterval(timer)
+        x = $(this).data('slide')
+
+        $('.slider-pagination-item').removeClass('pag-active')
+        $(this).addClass('pag-active')
+        slide.fadeOut(function () {
+            slide.css({ background: `url('${path + image[x]}')center/cover` })
+            slide.fadeIn()
+        })
+        timer = setInterval(change, 5000, 1)
+    })
+
+    $('#slider-next-btn')
+        .click(function (e) {
+            change(1)
+        })
+
+    $('#slider-prev-btn')
+        .click(function (e) {
+            change(-1)
+        })
+})
